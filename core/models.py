1e083e46ced4bb1fd1c19c160831653519f561b7
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-07-30 19:20:51 -0300

    Add necessary Models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+
+
+class MediaType(models.Model):
+    MediaTypeId = models.IntegerField(null=False, primary_key=True)
+    Name = models.CharField(max_length=120, null=True)
+
+    class Meta:
+        db_table = "MediaType"
+
+
+class Genre(models.Model):
+    GenreId = models.IntegerField(null=False, primary_key=True)
+    Name = models.CharField(max_length=120, null=True)
+
+    class Meta:
+        db_table = "Genre"
+
+
+class Artist(models.Model):
+    ArtistId = models.IntegerField(null=False, primary_key=True)
+    Name = models.CharField(max_length=120, null=True)
+
+    class Meta:
+        db_table = 'Artist'
+
+
+class Album(models.Model):
+    AlbumId = models.IntegerField(null=False, primary_key=True)
+    Title = models.CharField(max_length=160, null=False)
+    ArtistId = models.IntegerField(null=False)
+
+    class Meta:
+        db_table = 'Album'
+
+
+class Track(models.Model):
+    TrackId = models.IntegerField(null=False, primary_key=True)
+    Name = models.CharField(max_length=200, null=False)
+    AlbumId = models.IntegerField(null=False)
+    MediaTypeId = models.IntegerField(null=False)
+    GenreId = models.IntegerField(null=False)
+    Composer = models.CharField(max_length=200, null=True)
+    Milliseconds = models.IntegerField(null=False)
+    Bytes = models.IntegerField(null=True)
+    UnitPrice = models.FloatField(null=False)
+
+    class Meta:
+        db_table = 'Track'
